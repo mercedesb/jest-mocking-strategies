afa2d7b4560064375f275f26a2570a15fd175f3a
Mercedes Bernard <mercedesrbernard@gmail.com>
2020-07-07 09:20:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Jest mocking strategies</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mercedesb/jest-mock-examples"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h2>Code samples</h2>
         </a>
       </header>
+      <main>
+        <ul>
+          <li>Mock an entire node module</li>
+          <li>
+            Mock a single function of a node module, not worrying about behavior
+          </li>
+          <li>
+            Mock a single function of a node module, with specific behavior
+          </li>
+          <li>
+            Mock a single function of a node module multiple times in a test
+            file, with different behavior for each
+          </li>
+          <li>Mock default browser libraries for your whole test suite</li>
+          <li>Mock default browser libraries for a single test file</li>
+          <li>How a function is exported matters</li>
+        </ul>
+      </main>
     </div>
   );
 }
